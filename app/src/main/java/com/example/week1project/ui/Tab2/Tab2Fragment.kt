d7b0222e5beb554bf5b95b2450732978a38cbe1f
KKANGCHONG <leee0414@naver.com>
2024-12-31 20:33:02 +0900

    Tab3 - API done, except Tab1,2 merge ( A to F)

--- app/src/main/java/com/example/week1project/ui/Tab2/Tab2Fragment.kt
+++ app/src/main/java/com/example/week1project/ui/Tab2/Tab2Fragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.week1project.databinding.FragmentTab2Binding
+import com.example.week1project.databinding.FragmentTab21Binding
 
 class Tab2Fragment : Fragment() {
 
-    private var _binding: FragmentTab2Binding? = null
+    private var _binding: FragmentTab21Binding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,7 +25,7 @@ class Tab2Fragment : Fragment() {
         val tab2ViewModel =
             ViewModelProvider(this).get(Tab2ViewModel::class.java)
 
-        _binding = FragmentTab2Binding.inflate(inflater, container, false)
+        _binding = FragmentTab21Binding.inflate(inflater, container, false)
         val root: View = binding.root
 
 
